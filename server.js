ead7b9e97e939b67ef5c1387afb5b49889e31d5d
mrntando-dev <sibonginkosincust@gmail.com>
2025-11-10 07:21:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Ladybug Hosting running on port ${PORT}`);
 });
